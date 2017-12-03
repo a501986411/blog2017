51c7bd4d7f88c6ab88631a068128d7cf3b8478e6
a501986411 <501986411@qq.com>
2017-12-03 20:06:12 +0800

    友情链接管理

---
+++ application/admin/controller/FriendLink.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * 友情链接
+ * User: CHL
+ * Date: 2017/12/2
+ * Time: 15:26
+ */
+
+namespace app\admin\controller;
+
+class FriendLink extends Auth
+{
+
+    /**、
+     * 友情链接管理页面
+     * @return \think\response\View
+     */
+    public function index()
+    {
+        return view();
+    }
+
+
+    public function save()
+    {
+
+    }
+}
---
+++ application/admin/model/FriendLink.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: CHL
+ * Date: 2017/12/2
+ * Time: 15:44
+ */
+
+namespace app\admin\model;
+
+
+use think\Model;
+
+class FriendLink extends Model
+{
+    protected $autoWriteTimestamp = 'true';
+    
+
+}
