7f90952cf41bbc726c99eeb66cfe95f73753a734
a501986411 <501986411@qq.com>
2017-12-07 13:45:48 +0800

    kk

--- public/static/js/layui_use.js
+++ public/static/js/layui_use.js
@@ -3,7 +3,38 @@
  */
 var element;
 var laypage;
-layui.use(['element','laypage'], function(){
+layui.use(['element','laypage','util'], function(){
     element = layui.element;
     laypage = layui.laypage;
+    //分享工具
+    layui.util.fixbar({
+        bar1: '&#xe641;',
+        click: function (type) {
+            if (type === 'bar1') {
+                var sear = new RegExp('layui-hide');
+                if (sear.test($('.blog-share').attr('class'))) {
+                    shareIn();
+                } else {
+                    shareOut();
+                }
+            }
+        }
+    });
 });
+//显示百度分享
+function shareIn() {
+    $('.blog-share').unbind('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
+    $('.blog-share').removeClass('shareOut');
+    $('.blog-share').addClass('shareIn');
+    $('.blog-share').removeClass('layui-hide');
+    $('.blog-share').addClass('layui-show');
+}
+//隐藏百度分享
+function shareOut() {
+    $('.blog-share').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+        $('.blog-share').addClass('layui-hide');
+    });
+    $('.blog-share').removeClass('shareIn');
+    $('.blog-share').addClass('shareOut');
+    $('.blog-share').removeClass('layui-show');
+}
