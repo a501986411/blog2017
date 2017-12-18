fac9c67c376016f66285885b2c1bd8f2a039356e
a501986411 <501986411@qq.com>
2017-12-18 16:54:45 +0800

    a

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '118.190.209.51',
     // 数据库名
     'database'        => 'blog2017',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'blog',
     // 密码
-    'password'        => '',
+    'password'        => 'Blog2017!',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
